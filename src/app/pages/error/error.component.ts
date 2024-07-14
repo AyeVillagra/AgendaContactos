7b298429f0d9ea243c2d07ecb88a66b839848c13
AyeVillagra <ayelenvillagra@hotmail.com>
2024-07-14 18:08:47 -0300

    mejora page 404

--- src/app/pages/error/error.component.ts
+++ src/app/pages/error/error.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss'],
 })
-export class ErrorComponent {}
+export class ErrorComponent implements OnInit {
+  currentUserRole: string | null = null;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.currentUserRole = this.authService.getCurrentUserRole();
+  }
+
+  redirectToDefault(): void {
+    if (this.currentUserRole === 'Admin') {
+      this.router.navigate(['/admin']);
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
+}
